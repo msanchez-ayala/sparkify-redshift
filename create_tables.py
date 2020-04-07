e386db74db1e6d6102cee8945bc2f3a8e7c4cdaa
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-04-07 12:43:18 -0400

    add documentation for functions

--- create_tables.py
+++ create_tables.py
@@ -4,22 +4,49 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops all tables in the current database as denoted by the drop table
+    statements in sql_queries.py
+
+    Parameters
+    ----------
+    cur, conn: the cursor and connection objects associated with the connected
+        database.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates all tables in the current database as denoted by the create table
+    statements in sql_queries.py
+
+    Parameters
+    ----------
+    cur, conn: the cursor and connection objects associated with the connected
+        database.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Connects to the Redshift cluster specified in the config file. Calls
+    functions to drop all existing tables in the database, and then creates all
+    tables. Closes the connection to the cluster upon completion.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config['CLUSTER'].values()
+        )
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
--- etl.py
+++ etl.py
@@ -4,24 +4,51 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Copies data from JSON files in Udacity's S3 bucket to staging tables in
+    Redshift.
+
+    Parameters
+    ----------
+    cur, conn: the cursor and connection objects associated with the connected
+        database.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Inserts data from the staging tables into the fact and dimension tables.
+
+    Parameters
+    ----------
+    cur, conn: the cursor and connection objects associated with the connected
+        database.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Connects to the Redshift cluster specified in the config file. Calls
+    functions to load S3 data into staging tables and then inserts relevant data
+    into the fact and dimension tables. Closes the connection to the cluster
+    upon completion.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config['CLUSTER'].values()
+        )
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
